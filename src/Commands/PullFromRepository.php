4646feff63aa7f9e16f45c9178c09db026cee625
Jaaak15 <giacomojakzoffoli@gmail.com>
2025-01-27 15:06:17 +0100

    add

--- src/Commands/PullFromRepository.php
+++ src/Commands/PullFromRepository.php
@@ -24,7 +24,7 @@ class PullFromRepository extends Command
             return; 
         }
 
-        $result = Process::run("git pull origin master", function (string $type, string $output) {
+        $result = Process::run("git pull", function (string $type, string $output) {
               
             $this->info( $output );
 
